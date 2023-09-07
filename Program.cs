f57cb2dedeb86d5bc5a5e7193f259bd4fef47b31
jd <jd@jd.com>
2023-09-08 08:01:45 +1000

    removed boilerplate comments

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("What is your name?");
+﻿Console.WriteLine("What is your name?");
 var name = Console.ReadLine();
 var currentDate = DateTime.Now;
 Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
